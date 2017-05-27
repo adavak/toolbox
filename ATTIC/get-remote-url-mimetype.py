ef4cc850b43f8e242c3d1cc33eff47ec6484d19a
nodiscc <nodiscc@gmail.com>
2017-05-27 18:37:31 +0200

    reimport ATTIC directory: ATTIC/get-remote-url-mimetype.py

---
+++ ATTIC/get-remote-url-mimetype.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+'''
+get mimetype of remote document
+https://stackoverflow.com/questions/12474406/python-how-to-get-the-content-type-of-an-url
+'''
+
+res = urllib.urlopen("http://www.iana.org/assignments/language-subtag-registry" )
+http_message = res.info()
+full = http_message.type # text/plain
+main = http_message.maintype # text
