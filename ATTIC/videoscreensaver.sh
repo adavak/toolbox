4bc684bf2e97399bb18e76e7e3e1b31057c4351d
nodiscc <nodiscc@gmail.com>
2017-05-27 18:37:35 +0200

    reimport ATTIC directory: ATTIC/videoscreensaver.sh

---
+++ ATTIC/videoscreensaver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## setup MPlayer aruments, remove -nosound if you want the video
+## to play sound. If you have to specify the video driver to use
+## then add that to the list
+MPLAYERARGS="-nosound -nolirc -wid $XSCREENSAVER_WINDOW -nostop-xscreensaver -fs -really-quiet -loop 0"
+
+## path to video
+VIDEO=~/.config/videoscreensaver.avi
+
+exec mplayer $MPLAYERARGS "$VIDEO"
