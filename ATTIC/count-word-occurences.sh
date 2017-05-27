efe8afdcbe4ec394421244ea8bc5a2c3522fd101
nodiscc <nodiscc@gmail.com>
2017-05-27 18:37:31 +0200

    reimport ATTIC directory: ATTIC/count-word-occurences.sh

---
+++ ATTIC/count-word-occurences.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# http://www.unix.com/unix-dummies-questions-answers/87375-how-count-occurences-specific-word-file-bash-shell.html
+#Usage: count-word-occurences.sh "searchterm" file
+#Counts number of occurences of "word" in file
+tr -s ' ' '\n' < $2 | grep -c "$1"
