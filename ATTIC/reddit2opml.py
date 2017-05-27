7a9ac424a25436625fec6bbc44aede43824050ca
nodiscc <nodiscc@gmail.com>
2017-05-27 18:37:34 +0200

    reimport ATTIC directory: ATTIC/reddit2opml.py

---
+++ ATTIC/reddit2opml.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''
+    DESCRIPTION: backup reddit.com subscriptions/multireddits to multiple formats (TXT, OPML...)
+    USAGE: reddit-opml.py USERNAME PASSWORD
+'''
+
+import praw
+from pprint import pprint
+import sys
+from sys import argv
+
+user_agent = "Multireddit parser 0.1"
+user_name = sys.argv[1]
+password = sys.argv[2]
+r = praw.Reddit(user_agent=user_agent)
+r.login (user_name, password, disable_warning=True)
+
+print('####### MULTIS ########')
+multis = r.get_multireddits(user_name)
+for multi in multis:
+    print(multi.name)
+
+print("")
+
+for multi in multis:
+    combinedurl = 'https://www.reddit.com/r/'
+    print('######' + multi.name + '######')
+    subreddits = multi.subreddits
+    for subreddit in subreddits:
+        print(subreddit.display_name)
+        combinedurl = combinedurl + subreddit.display_name + '+'
+    print("")
+    print("Combined URL: " + combinedurl )
+    print("")
